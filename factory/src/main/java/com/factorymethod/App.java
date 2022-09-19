f782907c7b78ba3b035f619ae1066c15ad835a30
Andres <bravor.andres24@gmail.com>
2022-09-19 12:12:12 -0500

    Clase main, falta configurar coneccion

--- factory/src/main/java/com/factorymethod/App.java
+++ factory/src/main/java/com/factorymethod/App.java
@@ -1,13 +1,31 @@
 package com.factorymethod;
 
+import java.util.List;
+import java.sql.SQLException;
+import com.factorymethod.dao.ProductDAO;
+import com.factorymethod.entity.Product;
+
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Product productA = new Product(1L,"ProductoA",100);
+        Product productB = new Product(2L,"ProductoB",150);
+
+        ProductDAO productDAO = new ProductDAO();
+
+        productDAO.saveProduct(productA);
+        productDAO.saveProduct(productB);
+
+        List<Product> products = productDAO.findAllProducts();
+        System.out.println("productos:");
+        for (Product i:products)
+            System.out.println(i);
     }
 }
