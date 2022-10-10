da512b5170c837a3b663c66d91bbfb3c693363cb
Andres <bravor.andres24@gmail.com>
2022-10-09 20:21:03 -0500

    Proyecto patron singleton inicializado

---
+++ singleton/src/main/java/com/singleton/app/App.java
@@ -0,0 +1,15 @@
+package com.singleton.app;
+
+/**
+ * Hello world!
+ * @user: Andres
+ *
+ */
+
+
+public class App {
+    
+    public static void main( String[] args ){
+        System.out.println( "Hello World!" );
+    }
+}
