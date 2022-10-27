d6cf9a4a8e30813f757fc1124f3fc24b81c49451
Andrew <bravor.andres24@gmail.com>
2022-10-27 15:44:48 -0500

    models

--- builder/src/main/java/com/patronbuilder/app/App.java
+++ builder/src/main/java/com/patronbuilder/app/App.java
@@ -1,13 +1,26 @@
 package com.patronbuilder.app;
 
+import java.util.function.Function;
+
+import com.patronbuilder.app.Modelo.Persona;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+
+    public void modificar(){
+        Persona person1 = new Persona(new StringBuilder("Alvar0"), new StringBuilder("Jaramillo"), 23);
+        int edadPerson = person1.getEdad();
+        edadPerson = 78;
+        System.out.println(person1.getEdad());
+    }
+
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        App app = new App();
+        app.modificar();
     }
 }
