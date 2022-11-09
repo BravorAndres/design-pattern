5a838dd2913a74280f7c6025b44fcc9a7fdc3e23
Andrew <bravor.andres24@gmail.com>
2022-11-08 22:41:02 -0500

    Clone y deepClone completado

--- prototype/src/main/java/com/pattern/prototype/impl/PriceListImpl.java
+++ prototype/src/main/java/com/pattern/prototype/impl/PriceListImpl.java
@@ -4,15 +4,45 @@ package com.pattern.prototype.impl;
 import java.util.ArrayList;
 import java.util.List;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter 
 public class PriceListImpl implements IPrototype<PriceListImpl>{
     private String listName;
 
     private List<ProductItem> products = new ArrayList<ProductItem>();
 
+    public PriceListImpl(String listName){
+        this.listName = listName;
+    }
+
+    @Override
+    public PriceListImpl clone(){
+        PriceListImpl clone = new PriceListImpl(listName);
+        clone.setProducts(products);
+        return clone;
+    }
+
     @Override
     public PriceListImpl deepClone() {
-        // TODO Auto-generated method stub
-        return null;
+        List<ProductItem>  cloneProducts = new ArrayList<>();
+        for(ProductItem item: this.products){
+            ProductItem cloneItem = (ProductItem) item.clone();
+            cloneProducts.add(cloneItem);
+        }
+        PriceListImpl clone = new PriceListImpl(listName);
+        clone.setProducts(cloneProducts);
+        return clone;
+    }
+
+    @Override
+    public String toString(){
+        String items = "";
+        for (ProductItem item: this.products){
+            items += "\t"+item.toString()+"\n";
+        }
+        return "PriceListImpl{ListName = "+listName+", Products=\n"+items+"}";
     }
 
     
--- prototype/src/main/java/com/pattern/prototype/impl/ProductItem.java
+++ prototype/src/main/java/com/pattern/prototype/impl/ProductItem.java
@@ -1,6 +1,35 @@
 
 package com.pattern.prototype.impl;
 
-public class ProductItem {
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class ProductItem implements IPrototype<ProductItem>{
+    private String name;
+    private Double price;
+
+    public ProductItem(){}
+
+    public ProductItem(String name,double price){
+        this();
+        this.name = name;
+        this.price = price;
+    }
+
+    @Override
+    public ProductItem clone(){
+        return new ProductItem(this.name,this.price);
+    }
+
+    @Override
+    public ProductItem deepClone(){
+        return clone();
+    }
+
+    @Override
+    public String toString(){
+        return "productitem{name = "+name+" price = "+price+"}";
+    }
 
 }
